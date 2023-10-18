d10732f99e6e47c501f9cbeb5c170f56771d7201
KWONJYE <sheisdanmi@gmail.com>
2023-10-18 21:42:12 +0900

    기본편 - 싱글톤 컨테이너

---
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,17 @@
+package hello.core.singleton;
+
+public class StatefulService {
+
+//    private int price; // 상태를 유지하는 필드 10000 -> 20000
+    // 스프링 빈은 항상 무상태(stateless)로 설계!
+
+    public int order(String name, int price) {
+        System.out.println("name = " + name + "price = " + price);
+//        this.price = price; // 여기가 문제!
+        return price;
+    }
+
+//    public int getPrice() {
+//        return price;
+//    }
+}
