f3f8c588d895baf418112df28d639e57424db2d3
KWONJYE <sheisdanmi@gmail.com>
2024-05-20 20:11:35 +0900

    스프링 MVC 1편 - 응답 정적 리소스, 뷰 템플릿

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        return new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    /**
+     * 명시성이 너무 떨어지고 이렇게 딱 맞는 경우도 많이 없어서 권장하지 않음
+     */
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+}
